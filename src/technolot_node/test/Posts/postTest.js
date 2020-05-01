9aba1471889ea2994e2bf2bcdb413227f20dbd14
Joey <17821367+slowpoke1214@users.noreply.github.com>
2020-05-01 03:53:34 -0700

    Testing changes, completed

--- src/technolot_node/test/Posts/postTest.js
+++ src/technolot_node/test/Posts/postTest.js
@@ -5,30 +5,59 @@
 //To run the tests, make sure you have ran "npm install" to get the latest modules
 //Then just run "mocha --recursive" to run mocha and it will automatically test any file in the "test" directory
 
+//Imported dependencies
 const assert = require('chai').assert;
 const expect = require('chai').expect;
 const should = require('chai').should();
+
+const chaiHttp = require('chai-http');
+const chai = require('chai');
+const app = require('../../app.js');
+
+//Configuration of Chai
+chai.use(chaiHttp);
+chai.should();
 const PostController = require('../../Controllers/PostController');
 
 
 
 describe('Posts', function() {
+    var jwt = '';
+    var username = 'joek';
+    var password = '123';
 
-    it('should add two numbers', function () {
-        //This is just a demo of the unit testing for Mocha and Chai
-        expect(1 + 3).to.equal(4);
+    beforeEach( async () => {
+        res = await chai.request(app).post('/login')
+            .send({'username': username, 'password': password});
+        jwt = res.body.token;
     });
 
-    it('should test the creation of a Post', function () {
+    it('/POST', async () => {
+        const res = await chai.request(app).post('/post/addPost')
+            .set('Content-Type', 'application/json; charset=utf-8')
+            .set('Authorization', 'Bearer ' + jwt)
+            .send({'title': 'Test Title', 'content': 'Test Content', 'tag': 'Test Tag'});
 
+        //Tests to ensure correct content is received.
+        expect(res.body.post.title).to.equal('Test Title');
+        expect(res.body.post.content).to.equal('Test Content');
+        assert.include(res.body.post.tag, 'Test Tag', 'nice');
+
+        //Tests to ensure no error message occurs.
+        expect(res.body.errorMessage).to.equal('')
     });
 
-    it('should test viewing of Posts', function () {
-
+    it('/GET', async () => {
+        const res = await chai.request(app)
+            .get('/post/allPosts');
+        let posts = res.body;
+        let filteredResult = posts.filter(post => post.title == "Test Title");
+        expect(filteredResult.length > 0).to.be.true;
     });
 
-    it('should test Baos new comment thing', function () {
-
-    });
+    afterEach( async () => {
+        //Delete the posted item from database
+    })
 
 });
+
