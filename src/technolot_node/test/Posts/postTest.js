6910b5dda2f93c0ae4657a2dbb3315b044994aa5
Joey <17821367+slowpoke1214@users.noreply.github.com>
2020-05-01 03:56:36 -0700

    Changs

--- src/technolot_node/test/Posts/postTest.js
+++ src/technolot_node/test/Posts/postTest.js
@@ -1,9 +1,11 @@
-//This file is for unit testing.
-//Mocha is the testing library being used
-//Chai is the assertion library being used
+/*
+This file is for unit testing.
+Mocha is the testing library being used
+Chai is the assertion library being used
 
-//To run the tests, make sure you have ran "npm install" to get the latest modules
-//Then just run "mocha --recursive" to run mocha and it will automatically test any file in the "test" directory
+To run the tests, make sure you have ran "npm install" to get the latest modules.
+Then just run "mocha --recursive" to run mocha and it will automatically test any file in the "test" directory
+*/
 
 //Imported dependencies
 const assert = require('chai').assert;
@@ -17,7 +19,6 @@ const app = require('../../app.js');
 //Configuration of Chai
 chai.use(chaiHttp);
 chai.should();
-const PostController = require('../../Controllers/PostController');
 
 
 
