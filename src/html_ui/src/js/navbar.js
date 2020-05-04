6d8580d44e0d608a14b77a662f07fea4e9e25eae
bao <baobao5347@gmail.com>
2020-05-04 16:52:32 -0700

    make the search bar visible in all pages

--- src/html_ui/src/js/navbar.js
+++ src/html_ui/src/js/navbar.js
@@ -1,3 +1,23 @@
+//add search bar to nav column
+
+//create input element
+let input = document.createElement("input");
+input.type = "text";
+input.placeholder = "search here...";
+//create button element
+let button = document.createElement("button");
+button.innerText = "search";
+//put input and button elements into a div element
+let li = document.createElement("li");
+li.id="searchBar";
+li.appendChild(input);
+li.appendChild(button);
+//put li element into nav column, right after login/logout element
+let login = document.getElementById('navItemLogin');
+login.parentElement.appendChild(li)
+
+
+
 // Display Menu Item Based on login state
 if (sessionStorage.getItem('jwt')) {
     document.getElementById('navItemRegister').remove();
